0c70e459dbb752fe247c3ac4d3b5cd488de59b95
younext13 <sasha021303@gmail.com>
2021-11-27 15:30:56 +0300

    add basket

--- client/src/index.js
+++ client/src/index.js
@@ -9,10 +9,10 @@ export const Context = createContext(null)
 ReactDOM.render(
     <Context.Provider value={{
         user: new UserStore(),
-        device: new DeviceStore(),
+        device: new DeviceStore()
     }}>
         <App />
     </Context.Provider>,
-  document.getElementById('root')
+    document.getElementById('root')
 );
 
